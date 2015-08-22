ea3b4bb34b7d73a446a8261d24e061bc587057ce
Yvan Volochine <yvan.volochine@gmail.com>
2015-08-22 16:57:28 +0200

    js: fix menu selection for main page

--- js/main.js
+++ js/main.js
@@ -10,7 +10,13 @@ $(document).ready(function() {
 });
 
 function menuSelect(page) {
-  var links = $('#menu li a');
+  var links, abouts;
+
+  links = $('#menu li a');
+
+  // fix page name for GH pages
+  abouts = ['septentrion', 's3pt3ntrion.com', 'localhost:4000', '0.0.0.0:4000', ''];
+  if (abouts.indexOf(page) > -1) page = 'about';
 
   links.each(function() {
     var span = $(this).find('span').html().toLowerCase();
