81581dc82c0b3944820db84b8131478f4f4db7b6
Yvan Volochine <yvan.github@gmail.com>
2017-09-15 15:24:38 +0200

    new design polish

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+  var parallax = [
+    { id: '#actions', img: '/img/03-6-1920.jpg' },
+    { id: '#pastfuture', img: '/img/01-6-1920.jpg' },
+    { id: '#contact', img: '/img/01-4-1920.jpg' }
+  ];
+  parallax.forEach(function(el) {
+    $(el.id).parallax({ imageSrc: el.img });
+  });
   $(document).on('click', '[data-toggle="lightbox"]', function(e) {
     e.preventDefault();
     $(this).ekkoLightbox();
